7d88e064513b0d7f63c6e42f3051eabde6b4631e
unknown <oomtig@gmail.com>
2023-02-26 18:53:18 +0000

    Create f1 compute engine with IAP and SSH

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+/*
+
+
+Enable OS Login feature on VM ( this is added in metadata, and apparently required to be done at VM creation)
+Grant OS Login Permission to service account
+Grant IAP-Secured Tunnel User Permission
+Grant Cloud Build Service account(for cloud build)
+Enable IAP SSH traffic in firewall to the VM
+Enable IAM API
+Enable IAP Proxy API
+Enable OS Login API
+Cloud Build API(for cloudbuild)
+
+#VM with OS login metadata - IAP Secured tunnel permission
+resource
+*/
+
+
 #VPC
 resource "google_compute_network" "test_network" {
   project                 = "terraform-testing-project-sg"
@@ -27,19 +45,34 @@ resource "google_compute_firewall" "iap-ssh-allow" {
   target_tags = ["iap-ssh"]
 }
 
+# Create the f1-compute engine instance
+resource "google_compute_instance" "f1-instance" {
+  name         = "f1-instance"
+  machine_type = "f1-micro"
+  zone         = "us-west1-b"
+  tags         = ["iap-ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  # Enable OS Login and IAP SSH
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  network_interface {
+    network         = "test-network"
+    
+    # Do not allocate an external IP address
+    access_config   = []
+  }
+}
 
 
-/*
-Enable OS Login feature on VM ( this is added in metadata, and apparently required to be done at VM creation)
-Grant OS Login Permission to service account
-Grant IAP-Secured Tunnel User Permission
-Grant Cloud Build Service account(for cloud build)
-Enable IAP SSH traffic in firewall to the VM
-Enable IAM API
-Enable IAP Proxy API
-Enable OS Login API
-Cloud Build API(for cloudbuild)
-
-#VM with OS login metadata - IAP Secured tunnel permission
-resource
-*/
